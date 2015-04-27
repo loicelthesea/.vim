7c9e622c1ab241e5a162f92f1f65b559e3fb08b0
Chubyone <chubyone@outlook.com>
2015-04-27 13:41:51 -0300

    Improve bundles loader script's regex to only search inside '.git' folders

--- bundle/make_bundleload.sh
+++ bundle/make_bundleload.sh
@@ -1,1 +1,1 @@
-find ./ -name config -exec cat '{}' \; | grep url | sed 's/^.*url =/git clone/g' > bundle_load.sh && chmod +x bundle_load.sh
+find ./ -regex './.*/.git/config' -print -exec cat '{}' \; | grep url | sed 's/^.*url =/git clone/g' > bundle_load.sh && chmod +x bundle_load.sh
