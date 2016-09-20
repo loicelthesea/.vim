d91b13436f6b67ec81d91cf1b15f38e36c6e602e
Chubyone <Chubyone@outlook.com>
2016-09-20 15:30:02 -0300

    Add vim hybrid colorscheme

--- bundle.load.sh
+++ bundle.load.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/Shougo/unite.vim.git
-git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/tomasr/molokai
 git clone https://github.com/mattn/emmet-vim
+git clone https://github.com/vim-airline/vim-airline.git
+git clone https://github.com/w0ng/vim-hybrid
