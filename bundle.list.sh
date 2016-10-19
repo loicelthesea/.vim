25f525a2831041605d1762c469028fe9c716f76d
Chubyone <Chubyone@outlook.com>
2016-10-18 23:33:00 -0200

    - Update bundle list and loader

--- bundle.list.sh
+++ bundle.list.sh
@@ -1,1 +1,1 @@
-find ./ -regex './.*/.git/config' -print -exec cat '{}' \; | grep url | sed 's/^.*url =/git clone/g' > bundle.load.sh && chmod +x bundle.load.sh
+find ./ -regex './bundle/.*/.git/config' -print -exec cat '{}' \; | grep url | sed 's/^.*url =/git clone/g' > bundle.load.sh && chmod +x bundle.load.sh
--- bundle.load.sh
+++ bundle.load.sh
@@ -1,5 +1,3 @@
-git clone https://github.com/Shougo/unite.vim.git
-git clone https://github.com/tomasr/molokai
 git clone https://github.com/mattn/emmet-vim
-git clone https://github.com/vim-airline/vim-airline.git
-git clone https://github.com/w0ng/vim-hybrid
+git clone https://github.com/pangloss/vim-javascript.git
+git clone https://github.com/fatih/vim-go.git
