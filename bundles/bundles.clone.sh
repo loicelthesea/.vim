6c58e1512d9e158f3b0610cfa286157db38e4206
chubyone <chubyone@outlook.com>
2017-04-14 13:17:28 -0300

    Update scripts init & clone

--- bundles/bundles.clone.sh
+++ bundles/bundles.clone.sh
@@ -1,3 +1,5 @@
 git clone https://github.com/sjl/gundo.vim.git
 git clone https://github.com/mattn/emmet-vim
+git clone https://github.com/tomtom/tcomment_vim.git
+git clone https://github.com/godlygeek/csapprox.git
 git clone https://github.com/pangloss/vim-javascript.git
--- bundles/bundles.init.sh
+++ bundles/bundles.init.sh
@@ -1,1 +1,1 @@
-find ./ -regex './bundles/.*/.git/config' -print -exec cat '{}' \; | grep url | sed 's/^.*url =/git clone/g' > bundles.clone.sh && chmod +x bundles.clone.sh
+find ./ -regex './.*/.git/config' -print -exec cat '{}' \; | grep url | sed 's/^.*url =/git clone/g' > bundles.clone.sh && chmod +x bundles.clone.sh
