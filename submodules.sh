ed2a7b91de9d482963825c78de32fb94eb282e11
Loic El TheSea <loic.elthesea@outlook.com>
2017-12-18 12:51:14 +0100

    Update submodules.sh to git submodule add commands

--- submodules.sh
+++ submodules.sh
@@ -1,10 +1,10 @@
-git submodule init
-git submodule update
+# git submodule init
+# git submodule update
 
-# git submodule add https://github.com/sjl/gundo.vim.git
-# git submodule add https://github.com/tomtom/tcomment_vim.git
-# git submodule add https://github.com/mattn/emmet-vim
-# git submodule add https://github.com/pangloss/vim-javascript.git
-# git submodule add https://github.com/godlygeek/csapprox.git
-# git submodule add https://github.com/dracula/vim.git
-# git submodule add https://github.com/joshdick/onedark.vim
+git submodule add https://github.com/sjl/gundo.vim.git            ./bundle/gundo
+git submodule add https://github.com/tomtom/tcomment_vim.git      ./bundle/tcomment
+git submodule add https://github.com/mattn/emmet-vim.git          ./bundle/emmet
+git submodule add https://github.com/pangloss/vim-javascript.git  ./bundle/vim-javascript
+git submodule add https://github.com/godlygeek/csapprox.git       ./bundle/csapprox
+git submodule add https://github.com/dracula/vim.git              ./bundle/dracula-theme
+git submodule add https://github.com/joshdick/onedark.vim.git     ./bundle/onedark-theme
