932a0f071de4f120b4200c2ea571574d25d9ba4c
Loic El TheSea <loic.elthesea@outlook.com>
2017-12-18 13:18:54 +0100

    Remove csapprox from git module. Add submodules folders.

--- submodules.sh
+++ submodules.sh
@@ -1,10 +1,9 @@
-# git submodule init
-# git submodule update
+git submodule update --init
 
-git submodule add https://github.com/sjl/gundo.vim.git            ./bundle/gundo
-git submodule add https://github.com/tomtom/tcomment_vim.git      ./bundle/tcomment
-git submodule add https://github.com/mattn/emmet-vim.git          ./bundle/emmet
-git submodule add https://github.com/pangloss/vim-javascript.git  ./bundle/vim-javascript
-git submodule add https://github.com/godlygeek/csapprox.git       ./bundle/csapprox
-git submodule add https://github.com/dracula/vim.git              ./bundle/dracula-theme
-git submodule add https://github.com/joshdick/onedark.vim.git     ./bundle/onedark-theme
+# git submodule add https://github.com/sjl/gundo.vim.git            ./bundle/gundo
+# git submodule add https://github.com/tomtom/tcomment_vim.git      ./bundle/tcomment
+# git submodule add https://github.com/mattn/emmet-vim.git          ./bundle/emmet
+# git submodule add https://github.com/pangloss/vim-javascript.git  ./bundle/vim-javascript
+# git submodule add https://github.com/godlygeek/csapprox.git       ./bundle/csapprox
+# git submodule add https://github.com/dracula/vim.git              ./bundle/dracula-theme
+# git submodule add https://github.com/joshdick/onedark.vim.git     ./bundle/onedark-theme
